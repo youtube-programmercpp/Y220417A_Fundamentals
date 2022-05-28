cad2c86d7c1ecbb3975224bd39bc0f119f7b99d2
youtube-programmercpp <sbl908k@yahoo.co.jp>
2022-05-28 23:16:14 +0900

    知恵袋C言語回答活動報告

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <ctype.h>
+void trim_space(char* line)
+{
+	while (*line) {
+		if (isspace((unsigned char) * line)) {
+			for (char* 移動先 = line++;;) {
+				if (*line == '\0') {
+					*移動先 = '\0';
+					return;
+				}
+				else if (isspace((unsigned char)*line))
+					++line;
+				else
+					*移動先++ = *line++;
+			}
+		}
+		else
+			++line;
+	}
+}
+int main()
+{
+	puts("文字列を入力してください。");
+	char line[256];
+	if (fgets(line, sizeof line, stdin)) {
+		trim_space(line);
+		printf("%s\n", line);
+	}
+}
